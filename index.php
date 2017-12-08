dceaa83b806c8d7913586fd5fcb65aca706e43b6
Suporte3 <luksandrades18@gmail.com>
2017-12-08 17:44:05 -0200

    update

--- index.php
+++ index.php
@@ -29,20 +29,20 @@
     <body class="fixed-nav sticky-footer bg-dark" id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-            <a class="navbar-brand" href="index.html"><img class="img-fluid" style="vertical-align: middle; margin-left: 30px;" width="150" height="50" src="image/smoktech.png"></a>
+            <a class="navbar-brand" href="index.php"><img class="img-fluid" style="vertical-align: middle; margin-left: 30px;" width="150" height="50" src="image/smoktech.png"></a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                        <a class="nav-link" href="index.html">
+                        <a class="nav-link" href="index.php">
                             <i class="icons icon-home"></i>
                             <span class="nav-link-text">Início</span>
                         </a>
                     </li>
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
-                        <a class="nav-link" href="pages/sinais.html">
+                        <a class="nav-link" href="pages/sinais.php">
                             <i class="icons fa fa-download"></i>
                             <span class="nav-link-text">Sinais Recebidos</span>
                         </a>
@@ -198,7 +198,7 @@
                         </div>
                         <div class="modal-body">Tem certeza que quer sair?</div>
                         <div class="modal-footer">
-                            <a href="pages/login.html"><button class="btn btn-outline-primary">Sair</button></a>
+                            <a href="pages/login.php"><button class="btn btn-outline-primary">Sair</button></a>
                             <button class="btn btn-outline-secondary" type="button" data-dismiss="modal">Cancelar</button>                            
                         </div>
                     </div>
